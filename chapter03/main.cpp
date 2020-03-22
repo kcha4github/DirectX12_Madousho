6a22ad9959bce662edf5743340f77fefed9eeaef
Kunio Tsuge <kunio.tsuge@gmail.com>
2020-03-22 14:04:44 +0900

    ウィンドウ表示

--- chapter03/main.cpp
+++ chapter03/main.cpp
@@ -1,4 +1,5 @@
 #include <Windows.h>
+#include <tchar.h>
 #ifdef _DEBUG
 #include <iostream>
 #endif // _DEBUG
@@ -16,6 +17,20 @@ void DebugOutputFormatString(const char* format, ...)
 #endif // _DEBUG
 }
 
+// 面倒だけど書かなければいけない関数
+LRESULT WindowProcedure(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
+{
+	// ウィンドウが破棄されたら呼ばれる
+	if (msg == WM_DESTROY) {
+		PostQuitMessage(0);	// OSに対して「もうこのアプリは終わる」と伝える
+		return 0;
+	}
+	return DefWindowProc(hwnd, msg, wparam, lparam);	// 既定の処理を行う
+}
+
+const unsigned int window_width = 1280;
+const unsigned int window_height = 720;
+
 #ifdef _DEBUG
 int main()
 {
@@ -24,6 +39,56 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 {
 #endif // _DEBUG
 	DebugOutputFormatString("Show window test.");
-	getchar();
+
+	// ウィンドウクラスの生成＆登録
+	WNDCLASSEX w = {};
+
+	w.cbSize = sizeof(WNDCLASSEX);
+	w.lpfnWndProc = (WNDPROC)WindowProcedure;	// コールバック関数の指定
+	w.lpszClassName = _T("DX12Sample");			// アプリケーションクラス名
+	w.hInstance = GetModuleHandle(nullptr);		// ハンドルの取得
+
+	RegisterClassEx(&w);	// アプリケーションクラス
+
+	RECT wrc = { 0, 0, window_width, window_height };
+
+	// 関数を使ってウィンドウのサイズを補正する
+	AdjustWindowRect(&wrc, WS_OVERLAPPEDWINDOW, false);
+
+	// ウィンドウオブジェクトの生成
+	HWND hwnd = CreateWindow(w.lpszClassName,
+		_T("DX12テスト"),
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,			// 表示x座標はおまかせ
+		CW_USEDEFAULT,			// 表示y座標はおまかせ
+		wrc.right - wrc.left,	// ウィンドウ幅
+		wrc.bottom - wrc.top,	// ウィンドウ高さ
+		nullptr,				// 親ウィンドウハンドル
+		nullptr,				// メニューハンドル
+		w.hInstance,			// 呼び出しアプリケーションハンドル
+		nullptr);				// 追加パラメータ
+
+	// ウィンドウ表示
+	ShowWindow(hwnd, SW_SHOW);
+
+	MSG msg = {};
+	while (true)
+	{
+		if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+
+		// アプリケーションが終わるときにmessageがWM_QUITになる
+		if (msg.message == WM_QUIT)
+		{
+			break;
+		}
+	}
+
+	// もうクラスは使わないので登録解除する
+	UnregisterClass(w.lpszClassName, w.hInstance);
+
 	return 0;
 }
